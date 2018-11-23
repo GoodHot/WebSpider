3a905303d2fa8b0a61f02ff04e9d84b86a08a6ea
小王王 <wang@xiaowangwangdeMacBook-Pro.local>
2018-11-23 16:28:29 +0800

    config

---
+++ src/main/java/com/goodHot/fun/conf/WebsiteConfig.java
@@ -0,0 +1,7 @@
+package com.goodHot.fun.conf;
+
+public class WebsiteConfig {
+
+    public static final String ADMIN_WEBSITE = "http://localhost:8000";
+
+}
--- src/main/java/com/goodHot/fun/rest/admin/ArchiveRest.java
+++ src/main/java/com/goodHot/fun/rest/admin/ArchiveRest.java
@@ -2,14 +2,12 @@ package com.goodHot.fun.rest.admin;
 
 
 import com.goodHot.fun.common.RestResult;
+import com.goodHot.fun.conf.WebsiteConfig;
 import com.goodHot.fun.domain.Archive;
 import com.goodHot.fun.service.ArchiveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("archive")
 @RestController
@@ -19,6 +17,7 @@ public class ArchiveRest {
     private ArchiveService archiveService;
 
     @GetMapping("/page/{index}_{size}")
+    @CrossOrigin(WebsiteConfig.ADMIN_WEBSITE)
     public RestResult page(@PathVariable("index") Integer pageIndex, @PathVariable("size") Integer pageSize) {
         Page<Archive> page = archiveService.page(pageIndex, pageSize);
         return RestResult.ok(page);
