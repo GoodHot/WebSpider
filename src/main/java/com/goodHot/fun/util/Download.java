e565e55c9b06e03f44f9663f5460f6f1094aa1f2
小王王 <pause22@163.com>
2018-11-30 15:31:55 +0800

    增加异常注释

--- src/main/java/com/goodHot/fun/util/Download.java
+++ src/main/java/com/goodHot/fun/util/Download.java
@@ -1,14 +1,17 @@
 package com.goodHot.fun.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.*;
 import java.net.URL;
 
+@Slf4j
 public class Download {
 
     public static final int BUFFER_SIZE = 1024;
     public static final int EMPTY_BUFFER_FLAG = -1;
 
-    public void downloadFromUrl(String url, String filePath) {
+    public void downloadFromUrl(String url, String filePath) throws IOException {
         try (InputStream bufferIn = new BufferedInputStream(new URL(url).openStream());
              ByteArrayOutputStream bufferOut = new ByteArrayOutputStream();
              FileOutputStream fileOutputStream = new FileOutputStream(filePath);) {
@@ -25,11 +28,12 @@ public class Download {
             fileOutputStream.write(response);
 
         } catch (IOException e) {
-
+            log.error("[DownloadFile]down load fail -> url:{}, localPath:{}, error: {}", url, filePath, e.getMessage());
+            throw e;
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String url = "https://coubsecure-s.akamaihd.net/get/b100/p/coub/simple/cw_file/ab49b1c9d79/f786820f073ab03822511/muted_mp4_med_size_1542278618_muted_med.mp4";
         String filePath = "/Users/yanwenyuan/aCode4F/coubMP4Decode/src/main/resources/muted_mp4_med_size_1542278618_muted_med.mp4";
         new Download().downloadFromUrl(url, filePath);
