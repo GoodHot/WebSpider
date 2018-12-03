8f2fa6f34c6cb3c561503bd0bbb188be16f0e27a
小王王 <pause22@163.com>
2018-12-03 17:46:19 +0800

    post rest

---
+++ src/main/java/com/goodHot/fun/rest/web/PostRest.java
@@ -0,0 +1,42 @@
+package com.goodHot.fun.rest.web;
+
+import com.goodHot.fun.common.RestResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 内容
+ */
+@RestController
+@RequestMapping("post")
+public class PostRest {
+
+    /**
+     * 热门
+     * @return
+     */
+    @PostMapping("hot/{page}")
+    public RestResult hot() {
+        return RestResult.ok();
+    }
+
+    /**
+     * 最新
+     * @return
+     */
+    @PostMapping("fresh/{page}")
+    public RestResult fresh() {
+        return RestResult.ok();
+    }
+
+    /**
+     * 详情
+     * @return
+     */
+    @PostMapping("/detail/{id}")
+    public RestResult get() {
+        return RestResult.ok();
+    }
+
+}
