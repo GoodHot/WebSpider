4ac1fd3032b6f3c19796a3286b85ff8bea937913
阎文元 <yan95207396@126.com>
2018-12-28 15:16:27 +0800

    又拍云 - 操作

---
+++ src/main/java/com/goodHot/fun/util/upyun/com/upyun/SignatureListener.java
@@ -0,0 +1,5 @@
+package com.goodHot.fun.util.upyun.com.upyun;
+
+public interface SignatureListener {
+    String getSignature(String raw);
+}
---
+++ src/main/java/com/goodHot/fun/util/upyun/com/upyun/UpException.java
@@ -0,0 +1,7 @@
+package com.goodHot.fun.util.upyun.com.upyun;
+
+public class UpException extends Exception {
+    public UpException(String msg) {
+        super(msg);
+    }
+}
