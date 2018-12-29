3e258b573dfa45740e9f98a1ee5bbae09a1d8480
小王王 <pause22@163.com>
2018-12-29 13:50:02 +0800

    vote

--- src/main/java/com/goodHot/fun/rest/web/VoteWebRest.java
+++ src/main/java/com/goodHot/fun/rest/web/VoteWebRest.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("vote")
-public class VoteRest {
+public class VoteWebRest {
 
     @PostMapping("like")
     public RestResult like() {
