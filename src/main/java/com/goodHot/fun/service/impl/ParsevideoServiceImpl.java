9c37126a4931a527baeed7b10e253bf2cf0a110b
小王王 <pause22@163.com>
2019-03-08 15:31:23 +0800

    完善爬虫

--- src/main/java/com/goodHot/fun/service/impl/ParsevideoServiceImpl.java
+++ src/main/java/com/goodHot/fun/service/impl/ParsevideoServiceImpl.java
@@ -1,21 +1,49 @@
 package com.goodHot.fun.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.github.kevinsawicki.http.HttpRequest;
 import com.goodHot.fun.dto.rpc.Parsevideo;
 import com.goodHot.fun.service.ParsevideoService;
+import com.goodHot.fun.service.PhantomService;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class ParsevideoServiceImpl implements ParsevideoService {
 
+    @Autowired
+    private PhantomService phantomService;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    private static final String PARSE_VIDEO_WEB_ENCRPTY = "PARSE_VIDEO_WEB_ENCRPTY";
+
+    private static final Pattern pattern = Pattern.compile("var hash = \"(.+?)\"");
+
     @Override
     public String parse(String url) throws UnirestException {
+        ParseVideoEncrpty encrpty = null;
+        String json = redisTemplate.opsForValue().get(PARSE_VIDEO_WEB_ENCRPTY);
+        if(StringUtils.isBlank(json)){
+            encrpty = getEncrpty();
+            redisTemplate.opsForValue().set(PARSE_VIDEO_WEB_ENCRPTY, JSON.toJSONString(encrpty), 1, TimeUnit.HOURS);
+        } else {
+            encrpty = JSON.parseObject(json, ParseVideoEncrpty.class);
+        }
         HttpResponse<String> response = Unirest.post("https://www.parsevideo.com/api.php?callback=jQuery112409801861220671877_1551842302002")
                 .header("cache-control", "no-store, no-cache, must-revalidate,no-cache")
-                .header("cf-ray", "4b391315bffb9396-SJC")
+                .header("cf-ray", encrpty.ray)
                 .header("content-encoding", "br")
                 .header("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW")
                 .header("date", "Thu, 07 Mar 2019 02:17:42 GMT")
@@ -23,12 +51,12 @@ public class ParsevideoServiceImpl implements ParsevideoService {
                 .header("expires", "Thu, 19 Nov 1981 08:52:00 GMT")
                 .header("pragma", "no-cache")
                 .header("server", "cloudflare")
-                .header("set-cookie", "PHPSESSID=8f983afc71ac2c025563495a2409899d; path=/")
+                .header("set-cookie", encrpty.cookie)
                 .header("status", "200")
                 .header("vary", "Accept-Encoding")
                 .header("x-powered-by", "PHP/7.2.7")
                 .header("Postman-Token", "dcbdc312-8d56-4c9e-8c88-4633546bed70")
-                .body("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"url\"\r\n\r\n " + url + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"hash\"\r\n\r\n 478d45cd38188398ccdd6c4a33bc929f\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--")
+                .body("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"url\"\r\n\r\n " + url + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"hash\"\r\n\r\n " + encrpty.hash + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--")
                 .asString();
 
         String jsonp = response.getBody();
@@ -37,8 +65,30 @@ public class ParsevideoServiceImpl implements ParsevideoService {
         return videos.getVideo().get(0).getUrl();
     }
 
-    public static void main(String[] args) {
-        String jsonp = "jQuery112409801861220671877_1551842302002({\"pages\":0,\"total\":5,\"video\":[{\"url\":\"http:\\/\\/f.us.sinaimg.cn\\/000jPjmClx07s49PCQuY01041200jg7W0E010.mp4?label=mp4_ld&template=636x360.28.0&Expires=1551846586&ssig=7XPBHi4xSM&KID=unistore,video\",\"thumb\":\"\",\"desc\":\"STREAM_URL: \\u624e\\u9488\\u5415\\u62a4\\u58eb\\u7684\\u5fae\\u535a\\u89c6\\u9891\"},{\"url\":\"http:\\/\\/f.us.sinaimg.cn\\/003XB8EPlx07s49QET7q01041200s9XU0E010.mp4?label=mp4_hd&template=848x480.28.0&Expires=1551846586&ssig=YKo7frDIFe&KID=unistore,video\",\"thumb\":\"\",\"desc\":\"STREAM_URL_HD: \\u624e\\u9488\\u5415\\u62a4\\u58eb\\u7684\\u5fae\\u535a\\u89c6\\u9891\"},{\"url\":\"http:\\/\\/f.us.sinaimg.cn\\/000jPjmClx07s49PCQuY01041200jg7W0E010.mp4?label=mp4_ld&template=636x360.28.0&Expires=1551846586&ssig=7XPBHi4xSM&KID=unistore,video\",\"thumb\":\"\",\"desc\":\"MP4_SD_URL: \\u624e\\u9488\\u5415\\u62a4\\u58eb\\u7684\\u5fae\\u535a\\u89c6\\u9891\"},{\"url\":\"http:\\/\\/f.us.sinaimg.cn\\/003XB8EPlx07s49QET7q01041200s9XU0E010.mp4?label=mp4_hd&template=848x480.28.0&Expires=1551846586&ssig=YKo7frDIFe&KID=unistore,video\",\"thumb\":\"\",\"desc\":\"MP4_HD_URL: \\u624e\\u9488\\u5415\\u62a4\\u58eb\\u7684\\u5fae\\u535a\\u89c6\\u9891\"},{\"url\":\"http:\\/\\/f.us.sinaimg.cn\\/001eVFr9gx07s4awbSQf01041200njBc0E010.mp4?label=hevc_mp4_hd&template=848x480.32.0&Expires=1551846586&ssig=%2BD5qx6vllA&KID=unistore,video\",\"thumb\":\"\",\"desc\":\"HEVC_MP4_HD: \\u624e\\u9488\\u5415\\u62a4\\u58eb\\u7684\\u5fae\\u535a\\u89c6\\u9891\"}],\"status\":\"ok\"});";
-        System.out.println();
+    private ParseVideoEncrpty getEncrpty(){
+        ParseVideoEncrpty encrpty = new ParseVideoEncrpty();
+        phantomService.get("https://www.parsevideo.com/", (webDriver -> {
+            encrpty.hash = getHash(webDriver.getPageSource());
+            encrpty.cookie = phantomService.packCookie(webDriver.manage().getCookies());
+            HttpRequest req = HttpRequest.get("https://www.parsevideo.com/");
+            encrpty.ray = req.header("cf-ray");
+        }));
+        return encrpty;
     }
+
+    private String getHash(String source){
+        Matcher matcher = pattern.matcher(source);
+        if(matcher.find()){
+            return matcher.group(1);
+        }
+        return null;
+    }
+
+    @Data
+    public static class ParseVideoEncrpty {
+        private String cookie;
+        private String hash;
+        private String ray;
+    }
+
 }
